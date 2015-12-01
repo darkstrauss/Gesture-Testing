bc7910a50b1a7057d2511f2b145cc79c440d1089
ewout99 <ewout_speed@hotmail.com>
2015-12-02 00:12:23 +0100

    First Unity setup
    9 buttons outputting string of entered buttons, change color on enter
    and exit.

---
+++ Assets/Scripts/SphereParentCode.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class SphereParentCode : MonoBehaviour {
+
+    private List<int> Strike1 = new List<int>();
+    private List<int> Strike2 = new List<int>();
+    private List<int> Strike3 = new List<int>();
+
+    private string IDString;
+
+    public GameObject Temp;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+   
+	}
+
+    public void AddSphereID (int IDinput)
+    {
+        Strike1.Add(IDinput);
+    }
+
+    public void ReturnSphereIDs()
+    {
+        foreach (var item in Strike1)
+        {
+            IDString += item + " ";
+
+        }
+        Debug.Log("returing ID's:" + IDString);
+        IDString = "";
+        Strike1.Clear();
+
+
+    }   
+}
